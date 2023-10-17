576d4972e4e517f97e858891f76f81cc5a1762f2
Jaime Tejero <36772549+jaimemat17@users.noreply.github.com>
2023-10-17 14:13:03 +0200

    Added drag and drop

--- menu.js
+++ menu.js
@@ -1,12 +1,5 @@
-const {
-    app,
-    Menu,
-    shell,
-    ipcMain,
-    BrowserWindow,
-    globalShortcut,
-    dialog
-} = require('electron');
+const { app, Menu, shell, ipcMain, BrowserWindow, globalShortcut, dialog } = require('electron');
+const fs = require('fs');
 
 const template = [
     {
@@ -44,6 +37,25 @@ const template = [
                 }
             }
         ]
+    },
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Open',
+                accelerator: 'CommandOrControl+O',
+                click() {
+                    loadFile();
+                }
+            },
+            {
+                label: 'Save',
+                accelerator: 'CommandOrControl+S',
+                click() {
+                    saveFile();
+                }
+            }
+        ]
     }
 ];
 
@@ -75,12 +87,35 @@ ipcMain.on('editor-reply', (event, arg) => {
 });
 
 app.on('ready', () => {
-    globalShortcut.register('CommandOrControl+S', () => {
-        console.log('Saving the file');
-        const window = BrowserWindow.getFocusedWindow();
-        window.webContents.send('editor-event', 'save');
-    });
+    globalShortcut.register('CommandOrControl+S', () => { saveFile(); });
+    globalShortcut.register('CommandOrControl+O', () => { loadFile(); });
 });
+
+function saveFile() {
+    console.log('Saving the file');
+    const window = BrowserWindow.getFocusedWindow();
+    window.webContents.send('editor-event', 'save');
+}
+function loadFile() {
+    const window = BrowserWindow.getFocusedWindow();
+
+    const options = {
+        title: 'Pick a markdown file',
+        filters: [
+            { name: 'Markdown files', extensions: ['md'] },
+            { name: 'Text files', extensions: ['txt'] }
+
+        ]
+    };
+
+    dialog.showOpenDialog(window, options).then(result => {
+        if (result.filePaths && result.filePaths.length > 0) {
+            // read file and send to the renderer process
+            const content = fs.readFileSync(result.filePaths[0]).toString();
+            window.webContents.send('load', content);
+        }
+    });
+}
 
 ipcMain.on('save', (event, arg) => {
     console.log('Saving content of the file');
@@ -93,10 +128,13 @@ ipcMain.on('save', (event, arg) => {
             { name: 'MyFile', extensions: ['md'] },
         ]
     };
-    dialog.showSaveDialog(window, options, filename => {
-        console.log(filename);
+    dialog.showSaveDialog(window, options).then(result => {
+        console.log(`Saving content to the file: ${result.filePath}`);
+        fs.writeFileSync(result.filePath, arg);
     });
 });
+
+
 
 const menu = Menu.buildFromTemplate(template);
 
